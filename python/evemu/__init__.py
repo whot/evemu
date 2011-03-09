677154d179c6008e201bb3c67787863501b0b1b8
Duncan McGreggor <duncan@canonical.com>
2011-03-09 13:37:33 -0700

    * Added a wrapper class which is simply dedicated to wrapping the   non-device-specific funtions as cleanly as possible (with ctypes). * Cleaned up the main class which will duplicate the functionality of the   executable evemu scripts, but with methods. * Added a device abstraction class (the intention is to use the struct   referenced by the device pointer in addition to the device-specific evemu   functions). * Added custom exception class. * Added more unit tests.

--- python/evemu/__init__.py
+++ python/evemu/__init__.py
@@ -1,9 +1,165 @@
-from ctypes import CDLL
+import cStringIO
+import ctypes
 from ctypes.util import find_library
+import os
 import sys
+import tempfile
 
 
 LIB = "libutouch-evemu"
+
+
+# XXX move exceptions into their own module
+class WrapperError(Exception):
+    pass
+
+
+# XXX Move base classes and other supporting classes into their own module(s)
+class EvEmuBase(object):
+    """
+    A base wrapper class for the evemu functions, accessed via ctypes.
+    """
+    def __init__(self, library):
+        self._evemu = ctypes.CDLL(library)
+        self._libc = ctypes.CDLL(find_library("c"))
+
+
+class EvEmuDevice(EvEmuBase):
+    """
+    A wrapper class for the evemu device fucntions.
+    """
+    def __init__(self, library, device):
+        super(EvEmuDevice, self).__init__(library)
+        self._device = device
+
+    @property
+    def version(self):
+        pass
+
+    @property
+    def name(self):
+        pass
+
+    @property
+    def id_bustype(self):
+        pass
+
+    @property
+    def id_vendor(self):
+        pass
+
+    @property
+    def id_product(self):
+        pass
+
+    @property
+    def id_version(self):
+        pass
+
+    def abs_minimum(self, code):
+        pass
+
+    def abs_maximum(self, code):
+        pass
+
+    def abs_fuzz(self, code):
+        pass
+
+    def abs_flat(self, code):
+        pass
+
+    def abs_resolution(self, code):
+        pass
+
+    def abs_minimum(self, code):
+        pass
+
+    def has_prop(self, code):
+        pass
+
+    def has_event(self, event_type, code):
+        pass
+
+
+def checkdevice(function):
+    """
+    This is a decorator to be used for all methods that require the
+    ._device_pointer attribute to be set (in other words, by methods whose
+    evemu_* functions take a device pointer as a parameter).
+    """
+    def wrapped(self, *args, **kwds):
+        if not self._device_pointer:
+            raise WrapperError(
+                "No device name has been previously specifed with new().")
+    return wrapped
+
+
+class EvEmuWrapper(EvEmuBase):
+    """
+    A wrapper class for the evemu actions.
+    """
+    def __init__(self, library):
+        super(EvEmuWrapper, self).__init__(library)
+        self._device_pointer = None
+        self._device_fd, self._device_filename = tempfile.mkstemp()
+
+    def new(self, device_name):
+        """
+        @name: wanted input device name (or NULL to leave empty)
+        
+        This method allocates a new evemu device structure and initializes
+        all fields to zero. If name is non-null and the length is sane, it is
+        copied to the device name.
+
+        The device name will be used in the device data, similarly as is
+        presented by the device data that is viewable when you do the
+        following:
+          $ cat /proc/bus/input/devices
+        """
+        device_pointer = self._evemu.evemu_new(device_name)
+        self._device_pointer = ctypes.pointer(ctypes.c_int(device_pointer))
+        return self._device_pointer
+
+    @checkdevice
+    def read(self, filename):
+        # XXX this one's borked and needs to be re-examined
+        stream = self._libc.fopen(filename)
+            #ctypes.byref(ctypes.c_char_p(filename)), 
+            #ctypes.byref(ctypes.c_char_p("r")))
+        return self._evemu.evemu_new(
+            ctypes.byref(self._device_pointer),
+            stream)
+
+    def create(self):
+        pass
+
+    @checkdevice
+    def extract(self, filename):
+        input_fd = os.open(filename, os.O_RDONLY)
+        self._evemu.evemu_extract(
+            ctypes.byref(self._device_pointer), input_fd)
+        (output_fd, output_filename) = tempfile.mkstemp()
+        self._evemu.evemu_write(
+            ctypes.byref(self._device_pointer), output_fd)
+        return output_fd.read()
+
+    def delete(self):
+        pass
+
+    def destroy(self):
+        pass
+
+    def write_event(self):
+        pass
+
+    def read_event(self):
+        pass
+
+    def play(self):
+        pass
+
+    def record(self):
+        pass
 
 
 class EvEmu(object):
@@ -14,7 +170,7 @@ class EvEmu(object):
         """
         if not library:
             library = find_library(LIB)
-        self._evemu = CDLL(library)
+        self._wrapper = EvEmuWrapper(library)
         self._virtual_device = None
 
     def describe(self, path_to_touch_device):
@@ -26,18 +182,18 @@ class EvEmu(object):
         Scripts that use this method need to be run as root.
         """
         fd = open(path_to_touch_device, "r")
-        device = self._evemu.evemu_new(0)
+        device = self._wrapper.evemu_new(0)
         # XXX check for device being not none, err out if so
-        data = self._evemu.evemu_extract(device, fd)
+        data = self._wrapper.evemu_extract(device, fd)
         # XXX check for data being not none, err out if so
         fd.close()
         # XXX I don't like writing to stdout by default with a library. I'd
-        # prever this be an option. For now, we'll duplicate the scripts and
-        # keep it as is...
-        self._evemu.evemu_write(device, sys.stdout)
+        # prefer that this were an option. For now, we'll duplicate the scripts
+        # and keep it as is...
+        self._wrapper.evemu_write(device, sys.stdout)
         return data
 
-    def create_device(self, description_file):
+    def create_device(self, device_description_file):
         """
         The create_device method creates a virtual input device based on the
         provided description-file. This description will have been created by
@@ -46,6 +202,8 @@ class EvEmu(object):
 
         Scripts that use this method need to be run as root.
         """
+        if self._virtual_device:
+            return self._virtual_device
 
     def record(self):
         """
@@ -57,7 +215,7 @@ class EvEmu(object):
         Scripts that use this method need to be run as root.
         """
 
-    def play(self, events_file):
+    def play(self, device_description_file="", events_file=""):
         """
         The play method replays the event sequence, as provided by the
         events-file, through the input device. The event sequence must be in
@@ -65,6 +223,8 @@ class EvEmu(object):
 
         Scripts that use this method need to be run as root.
         """
-
+        device_fd = os.open(device_description_file, os.O_WRONLY)
+        events_fd = os.open(events_file, os.O_WRONLY)
+        self._wrapper.play(device_fd, events_fd)
 
 
--- python/evemu/tests/test_evemu.py
+++ python/evemu/tests/test_evemu.py
@@ -1,52 +1,135 @@
 from ctypes.util import find_library
 import os
+import tempfile
 import unittest
 
-from evemu import LIB, EvEmu
+from evemu import (
+    LIB, EvEmu, EvEmuBase, EvEmuDevice, EvEmuWrapper, WrapperError)
 
 
 # This needs to get updated with new versions of evemu
 LOCAL_LIB = "../src/.libs/libutouch-evemu.so.1.0.0"
+
 # This should also be examined every release of evemu
 API = [
     "evemu_new",
+    "evemu_delete",
     "evemu_extract",
     "evemu_write",
     "evemu_read",
+    "evemu_write_event",
+    "evemu_record",
+    "evemu_read_event",
+    "evemu_play",
     "evemu_create",
     "evemu_destroy",
-    "evemu_delete",
-    "evemu_write_event",
-    "evemu_read_event",
-    "evemu_record",
-    "evemu_play"]
+    # Device functions
+    "evemu_get_version",
+    "evemu_get_name",
+    "evemu_get_id_bustype",
+    "evemu_get_id_vendor",
+    "evemu_get_id_product",
+    "evemu_get_id_version",
+    "evemu_get_abs_minimum",
+    "evemu_get_abs_maximum",
+    "evemu_get_abs_fuzz",
+    "evemu_get_abs_flat",
+    "evemu_get_abs_resolution",
+    "evemu_has_prop",
+    "evemu_has_event",
+    ]
 
 
-class EvEmuTestCase(unittest.TestCase):
+class BaseTestCase(unittest.TestCase):
 
     def setUp(self):
         library = find_library(LIB)
         if not library:
             library = LOCAL_LIB
-        self.evemu = EvEmu(library=library)
+        self.library = library
+        self.device_name = "My Special Device"
         from evemu import tests
         basedir = tests.__path__[0]
-        self.data_dir = os.path.join(basedir, "data")
+        self.data_dir = os.path.join(basedir, "data", "ntrig-xt2")
 
-    def _get_device_file(self):
-        return os.path.join(self.data_dir, "device.prop")
+    def get_device_file(self):
+        return os.path.join(self.data_dir, "ntrig-xt2.device")
 
-    def _get_events_file(self):
-        return os.path.join(self.data_dir, "gesture.events")
+    def get_events_file(self):
+        return os.path.join(self.data_dir, "ntrig-xt2-4-tap.event")
 
     def test_initialize(self):
+        wrapper = EvEmuBase(self.library)
         # Make sure that the library loads
         self.assertNotEqual(
-            self.evemu._evemu._name.find("libutouch-evemu"), -1)
+            wrapper._evemu._name.find("libutouch-evemu"), -1)
         # Make sure that the expected functions are present
         for function_name in API:
-            function = getattr(self.evemu._evemu, function_name)
+            function = getattr(wrapper._evemu, function_name)
             self.assertTrue(function is not None)
+
+
+class EvEmuDeviceTestCase(BaseTestCase):
+
+    def setUp(self):
+        super(EvEmuDeviceTestCase, self).setUp()
+        self.device = EvEmuDevice(self.library, self.device_name)
+
+    def test_initialize(self):
+        self.assertTrue(self.device._device, self.device_name)
+
+
+class EvEmuWrapperTestCase(BaseTestCase):
+
+    def setUp(self):
+        super(EvEmuWrapperTestCase, self).setUp()
+        self.wrapper = EvEmuWrapper(self.library)
+
+    def test_initialize(self):
+        self.assertTrue(self.wrapper._device_pointer is None)
+
+    def test_new(self):
+        self.wrapper.new(self.device_name)
+        pointer = self.wrapper._device_pointer
+        self.assertTrue(isinstance(pointer.contents.value, int))
+
+    def test_read_no_device_pointer(self):
+        filename = tempfile.mkstemp()
+        self.assertRaises(WrapperError, self.wrapper.read, filename)
+
+    def test_read(self):
+        self.wrapper.new(self.device_name)
+        # hrm... not sure if I should be reading from the device file or
+        # preping an empty file...
+        result = self.wrapper.read(self.get_device_file())
+        # XXX need to do checks against the result
+
+    # XXX file this test in
+    def test_create_no_device_pointer(self):
+        pass
+
+    # XXX file this test in
+    def test_create(self):
+        pass
+
+    def test_extract_no_device_pointer(self):
+        filename = tempfile.mkstemp()
+        self.assertRaises(WrapperError, self.wrapper.extract, filename)
+
+    def test_extract(self):
+        self.wrapper.new(self.device_name)
+        result = self.wrapper.read(self.get_device_file())
+        print result
+
+class EvEmuTestCase(BaseTestCase):
+
+    def setUp(self):
+        super(EvEmuTestCase, self).setUp()
+        self.evemu = EvEmu(library=self.library)
+
+    def test_initialize(self):
+        self.assertTrue(self.evemu._wrapper is not None)
+        self.assertTrue(self.evemu._virtual_device is None)
 
     def test_describe(self):
         pass
@@ -59,7 +142,7 @@ class EvEmuTestCase(unittest.TestCase):
         pass
 
     def test_play(self):
-        # Load the device file
+        #self.evemu.play(self.get_events_file(), self.get_device_file())
         pass
 
 
