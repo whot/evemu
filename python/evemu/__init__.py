8fc2605342743dd1aff3756ad8098b92e0c8857a
Duncan McGreggor <duncan@canonical.com>
2011-03-08 21:34:55 -0700

    Started working on the decribe method.

--- python/evemu/__init__.py
+++ python/evemu/__init__.py
@@ -1,5 +1,6 @@
 from ctypes import CDLL
 from ctypes.util import find_library
+import sys
 
 
 LIB = "libutouch-evemu"
@@ -21,8 +22,9 @@ class EvEmu(object):
         if not library:
             library = find_library(LIB)
         self._evemu = CDLL(library)
+        self._virtual_device = None
 
-    def describe(self):
+    def describe(self, path_to_touch_device):
         """
         The describe gathers information about the input device and prints it
         to stdout. This information can be parsed by the create_device to
@@ -30,13 +32,24 @@ class EvEmu(object):
 
         Scripts that use this method need to be run as root.
         """
+        fd = open(path_to_touch_device, "r")
+        device = self._evemu.evemu_new(0)
+        # XXX check for device being not none, err out if so
+        data = self._evemu.evemu_extract(device, fd)
+        # XXX check for data being not none, err out if so
+        fd.close()
+        # XXX I don't like writing to stdout by default with a library. I'd
+        # prever this be an option. For now, we'll duplicate the scripts and
+        # keep it as is...
+        self._evemu.evemu_write(device, sys.stdout)
+        return data
 
     def create_device(self, description_file):
         """
         The create_device method creates a virtual input device based on the
-        provided description-file. This description is usually created by the
-        describe method. create_device then creates a new input device with
-        uinput and prints the name and the device file to stdout.
+        provided description-file. This description will have been created by
+        the describe method. The create_device method then creates a new input
+        device with uinput and prints the name and the device file to stdout.
 
         Scripts that use this method need to be run as root.
         """
