e489ab910e94173efd0abe3af6c6c45b669bb2a1
Duncan McGreggor <duncan@ubuntu.com>
2011-03-14 20:30:44 -0600

    Tweaked the display of test results.

--- python/evemu/testing.py
+++ python/evemu/testing.py
@@ -72,21 +72,29 @@ class CustomTestResult(TextTestResult):
 
     def __init__(self, *args, **kwds):
         super(CustomTestResult, self).__init__(*args, **kwds)
-        self.current_module_and_class = ""
-        self.last_module_and_class = ""
+        self.current_module = ""
+        self.last_module = ""
+        self.current_class = ""
+        self.last_class = ""
 
     def startTest(self, test):
         unittest.TestResult.startTest(self, test)
         if not self.showAll:
             return
-        self.last_module_and_class = self.current_module_and_class
+        self.last_module = self.current_module
+        self.last_class = self.current_class
         method = test._testMethodName
         module_and_class = test.id().rsplit(method)[0][:-1]
-        self.current_module_and_class = module_and_class
-        if self.last_module_and_class != self.current_module_and_class:
-            heading = "\n%s.%s" % (util.get_test_module(), module_and_class)
+        this_module = ".".join(module_and_class.split(".")[:-1])
+        self.current_module = this_module
+        this_class = module_and_class.split(".")[-1]
+        self.current_class = this_class
+        if self.last_module != self.current_module:
+            heading = "\n%s.%s" % (util.get_test_module(), this_module)
             self.stream.writeln(heading)
-        self.stream.write("\t%s " % method.ljust(50, "."))
+        if self.last_class != self.current_class:
+            self.stream.writeln("    %s" % this_class)
+        self.stream.write("        %s " % method.ljust(50, "."))
         self.stream.write(" ")
         self.stream.flush()
 
