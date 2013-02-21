56ee2eea0af3e07b6b8fb9325bd42f98af834b26
Benjamin Tissoires <benjamin.tissoires@gmail.com>
2013-02-21 16:29:56 +0100

    remove -D_GNU_SOURCE=1 from CFLAGS
    The macro _GNU_SOURCE is only required in evemu-event. So remove
    it from the global CFLAGS and add it directly in the required
    source file.
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@gmail.com>

--- tools/evemu-event.c
+++ tools/evemu-event.c
@@ -18,6 +18,8 @@
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  ****************************************************************************/
+#define _GNU_SOURCE
+
 #include "evemu.h"
 #include <getopt.h>
 #include <limits.h>
